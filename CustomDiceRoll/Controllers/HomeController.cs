e822f113eed5de2e0d436a95e1b5ed498b554d5c
hzlmnd <52181893+hazelmind@users.noreply.github.com>
2019-11-20 16:55:18 -0500

    Create dice size, roll any amount, and view roll results.

---
+++ CustomDiceRoll/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CustomDiceRoll.Models;
+
+namespace CustomDiceRoll.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<int> newList;
+
+        public List<int> GetNewList()
+        {
+            return newList;
+        }
+
+        public void SetNewList(List<int> value)
+        {
+            newList = value;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Roll(int sides, int rolls)
+        {
+            int i = 0;
+
+            while (i < rolls)
+            {
+                i++;
+                Random random = new Random();
+                int newDice = random.Next(1, sides + 1);
+                AddRoll(newDice);
+            }
+            return View(GetNewList());
+        }
+
+        public List<int> AddRoll(int newDice)
+        {
+            List<int> ResultList = new List<int>();
+            if (GetNewList() != null)
+            {
+                ResultList = GetNewList();
+            }
+            ResultList.Add(newDice);
+            SetNewList(ResultList);
+            return ResultList;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CustomDiceRoll/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomDiceRoll.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
